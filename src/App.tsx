a5b08192e59dbaedd3c67b43ab2191718d1e65c3
spacecodeur <spacecodeur@gmail.com>
2024-11-23 18:16:20 +0100

    first implement of front website with some configs and linters

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>Spacerework front</h1>;
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+
+const rootElement = document.getElementById("root");
+
+if (rootElement === null) {
+  throw new Error(`Your HTML Document should contain a <div id="root"></div>`);
+}
+
+createRoot(rootElement).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
